249140f4a95697797e048e78ddc0e790a5db6a9e
Mathias R <mathias.ramir@gmail.com>
2026-05-16 11:03:22 -0300

    Creacion de la clase GestorPersistencia que se encarga de la gestion de persistencia del programa

---
+++ src/persistencia/GestorPersistencia.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package persistencia;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import modelo.OLT;
+
+public class GestorPersistencia {
+    private String nombreArchivo;//Archivo.dat
+
+    public GestorPersistencia(String nombreArchivo) {
+        this.nombreArchivo = nombreArchivo;
+    }
+    
+    public void guardar(OLT olt){
+        try(ObjectOutputStream salida = new ObjectOutputStream(new FileOutputStream(nombreArchivo))){
+            salida.writeObject(olt);
+            salida.close();
+        } catch (IOException ex) {
+            System.out.println("Error: " + ex.getLocalizedMessage());
+        }
+    }
+    
+    public OLT cargar() {
+        try(ObjectInputStream entrada = new ObjectInputStream(new FileInputStream(nombreArchivo))) {
+            OLT olt = (OLT) entrada.readObject();
+            entrada.close();
+            return olt;
+        }catch(IOException|ClassNotFoundException e){
+            System.out.println("Error: " + e.getLocalizedMessage());
+        }
+        return null;
+    }
+    
+    
+}
